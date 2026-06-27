3e93bd4802dffa06d6eb0e6c5e98e937ed72140d
Zach Thieme <zach@techsage.org>
2026-06-26 18:02:07 -0700

    feat: PostgreSQL migration files with golang-migrate

---
+++ migrations/000001_initial_schema.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS competency_ratings;
+DROP TABLE IF EXISTS notifications;
+DROP TABLE IF EXISTS feedback;
+DROP TABLE IF EXISTS interviews;
+DROP TABLE IF EXISTS interview_loops;
+DROP TABLE IF EXISTS competencies;
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS users;
